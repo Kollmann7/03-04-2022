7f095fbeeffbe4b665d2a18814888c1ad18226d0
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-05-06 15:12:53 +0200

    change dropdown location

--- scripts/selector.js
+++ scripts/selector.js
@@ -1,5 +1,23 @@
-class Selector {
-constructor(selectItemCallback) {
-    this.selectItemCallback = selectItemCallback
-    }
+import ApplianceSelector from './appliance.js'
+import IngredientSelector from './ingredient.js'
+import UstensilSelector from './ustensils.js'
+
+
+
+
+export default class Selector {
+    constructor(selectItemCallback) {
+        this.selectItemCallback = selectItemCallback
+        }
+    // dropdownsDOM(){
+    //     const dropdowns = document.querySelector('dropdown-menu')
+    //     dropdowns.innerHTML = `<li><a class="dropdown-item" href="#">Action</a></li>`
+        
+    // }
+
+        
 }
+
+
+// const selector = new Selector
+// selector.dropdownsDOM()
