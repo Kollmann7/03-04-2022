e77e5203814dccb1c8ace3afcbd29ae8a2ec2159
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-06-02 11:44:05 +0200

    Add selector DOM

--- scripts/selector.js
+++ scripts/selector.js
@@ -1,26 +1,53 @@
-import ApplianceSelector from './appliance.js'
-import IngredientSelector from './ingredient.js'
-import UstensilSelector from './ustensils.js'
-
+import recipes from './data/recipes.js'
+import UstensilSelector from "./ustensils.js"
 
 
 
 export default class Selector {
     constructor(selectItemCallback) {
         this.selectItemCallback = selectItemCallback
-        }
-    dropdownsDOM(){
-        const dropdowns = document.querySelector('dropdown-menu')
-        dropdowns.innerHTML = `<li><a class="dropdown-item" href="#">Sucre</a></li>`
+        this.ustensils = ['saladier', 'poele', 'four']        
+    }
+    selectorDOM(){
+        const dropdowns = document.getElementById('dropdowns-container')
+    
+        const btnGroup = document.createElement('div')
+        btnGroup.className = 'btn-group'
+
+        const button = document.createElement('button')
+        button.className = 'btn btn-success dropdown-toggle'
+        button.setAttribute('data-bs-toggle',"dropdown")
+        button.type = 'button'
+        button.textContent = 'Ustensil'
+
+        const ul = document.createElement('ul')
+        ul.className = 'dropdown-menu'
+
+        const btn = document.createElement('button')
+        btn.className = 'dropdown-item'
+        btn.textContent = 'saladier'
+
+
+        this.ustensils.forEach((ustensil) => {
+            const li = document.createElement('li')
+            li.textContent = ustensil
+            ul.appendChild(li)
+            li.addEventListener('click', (e) => {
+                this.selectedTag(ustensil)
+            })
+
+        })
+        
+       
+              
+        btnGroup.append(button,ul)
+        dropdowns.append(btnGroup )
+    
         return dropdowns
+    }  
+    selectedTag(ustensil){
+        this.selectItemCallback({type:'ustensil', label:ustensil})
         
     }
-    getDOM(){
-        return this.dropdownsDOM()
-    }
-        
+   
 }
-
-
-// const selector = new Selector
-// selector.dropdownsDOM()
