e08472f9a9782a4c2b91840e3adefcf17a3e9128
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-06-02 11:45:34 +0200

    Delete selectorDOM in abstract

--- scripts/abstractSelector.js
+++ scripts/abstractSelector.js
@@ -5,26 +5,12 @@ export default class AbstractSelector {
       this.loadRecipes(recipes)
       this.type = type
       this.selectCallback = selectCallback
+      this.displaySelectableItems()
 
     }
     
     loadRecipes(recipes) {
       throw new Error('loadRecipes should be implemented')
-    }
-    SelectorDOM(){
-      const dropdowns = document.querySelector('dropdowns')
-
-      const btnGroup = document.createElement('div')
-      btnGroup.className = 'btn-group'
-      const button = document.createElement('button')
-      button.className = 'btn btn-primary dropdown-toggle'
-      button.type = 'button'
-      const ul = document.createElement('ul')
-      ul.className = 'dropdown-menu'
-
-      dropdowns.append(btnGroup, button, ul)
-
-      // return dropdowns
     }
     displaySelectableItems() {
       console.log(this.type, 'list :', this.itemsList)
@@ -32,3 +18,4 @@ export default class AbstractSelector {
   }
 
 console.log(recipes)
+
