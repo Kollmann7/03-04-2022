8c8fd0f280f028461ee16bed677a32f24737912e
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-06-02 16:28:41 +0200

    add bounce and searchbar filter

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,49 @@
 import recipes from './data/recipes.js'
 import RecipeList from "./recipeList.js"
-import AppliancesList from "./appliance.js"
-import UstensilList from "./ustensils.js"
-import IngredientList from "./ingredient.js"
+import ApplianceSelector from "./appliance.js"
+import UstensilSelector from "./ustensils.js"
+import IngredientSelector from "./ingredient.js"
 import TagList from "./selectTags.js"
 import Selector from './selector.js'
 
 class Page {
   constructor() {
-    this.tagList = new TagList(this.onRemoveTag)
+    this.recipes = recipes
+    this.tagList = new TagList(this.onRemoveTag.bind(this))
     this.recipeList = new RecipeList()
-    this.ustensilSelector = new Selector(this.addTag)
+    this.ustensilSelector = new UstensilSelector(recipes, 'Ustensil')
+    this.applianceSelector = new ApplianceSelector(recipes, 'Appliance')
+    this.ingredientSelector = new IngredientSelector(recipes, 'Ingredient')
+    this.selector = new Selector(this.onAddTag.bind(this), this.ustensilSelector)
+    const searchBarForm = document.getElementById('form1')
+    searchBarForm.addEventListener('input', this.onSearchBar.bind(this))
+    this.selector.selectorDOM()
+    this.tagList.displayTagsDOM()
+    this.refresh()
   }
   
   refresh() {
-    // Récupérer les filtres (tags + searchBar)
-
-    const searchBarForm = document.getElementById('form1')
-    searchBarForm.addEventListener('input', this.recipeList.filtering)
-    console.log(this.recipeList.filtering)
-
-    // const recipeListfilter = this.recipeList.filtering(tagsAppliances, tagsUstensil, tagsIngredient)
-
-    // const ustensilSelector = this.ustensilSelector.loadRecipes(this.recipeList)
+    const recipeListfilter = this.recipeList.filtering(this.tagList.tags, this.searchBarValue)
+  }
+  
+  onSearchBar(e){
+    this.searchBarValue = e.target.value
     
+    clearTimeout(this.timeOutId)
+    this.timeOutId = setTimeout(this.refresh.bind(this), 300)
   }
-  onRemoveTag(){
+  
+  onRemoveTag(tag){
+    this.tagList.removeTag(tag)
     this.refresh()
   }
-  addTag(tag) {
-    this.tagList.addTag(tag)
-    this.refresh()
+
+  onAddTag(tag) {
+    if (!this.tagList.tags.some(t => t.label === tag.label)){
+      this.tagList.addTag(tag)
+      this.refresh()
+    }
   }
 }
 
 const mainPage = new Page 
-mainPage.refresh()
