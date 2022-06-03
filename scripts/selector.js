4dd6173a495d4faa25cb44c0c057808be9f4ffe4
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-06-03 13:28:33 +0200

    add searchinput in selector

--- scripts/selector.js
+++ scripts/selector.js
@@ -1,12 +1,10 @@
 import recipes from './data/recipes.js'
-import UstensilSelector from "./ustensils.js"
-
 
 
 export default class Selector {
-    constructor(selectItemCallback) {
+    constructor(selectItemCallback, ustensilsSelector) {
         this.selectItemCallback = selectItemCallback
-        this.ustensils = ['saladier', 'poele', 'four']        
+        this.ustensils = ustensilsSelector.itemsList     
     }
     selectorDOM(){
         const dropdowns = document.getElementById('dropdowns-container')
@@ -16,38 +14,59 @@ export default class Selector {
 
         const button = document.createElement('button')
         button.className = 'btn btn-success dropdown-toggle'
-        button.setAttribute('data-bs-toggle',"dropdown")
+        button.setAttribute('data-bs-toggle','dropdown')
         button.type = 'button'
-        button.textContent = 'Ustensil'
+
+        const textSelector = document.createElement('span')
+        textSelector.textContent = 'Ustensil'
+
+        button.appendChild(textSelector)
+        button.addEventListener('click', () => {
+            console.log(input.value)
+            if(textSelector.style.visibility  === 'hidden' && input.value === null){
+                textSelector.style.visibility  = 'visible'
+            }else{
+                textSelector.style.visibility  = 'hidden'
+            }
+        })
+
+        
+        const input = document.createElement('input')
+        input.type = 'text'
+        input.placeholder = 'Recherche un ustensil'
+        input.className = 'input-selector'
+        button.addEventListener('input', () => {
+            Array.from(ul.childNodes).forEach((listItem) => {
+                const item = listItem.textContent.toLowerCase();
+                if (!item.includes(input.value)) {
+                    listItem.style.display = 'none'
+                } else {
+                    listItem.style.display = 'block'
+                }
+            })
+        })
+
+        button.appendChild(input)
 
         const ul = document.createElement('ul')
         ul.className = 'dropdown-menu'
-
-        const btn = document.createElement('button')
-        btn.className = 'dropdown-item'
-        btn.textContent = 'saladier'
-
-
+ 
         this.ustensils.forEach((ustensil) => {
             const li = document.createElement('li')
             li.textContent = ustensil
+            li.className = 'dropdown-item'
             ul.appendChild(li)
-            li.addEventListener('click', (e) => {
+            li.addEventListener('click', () => {
                 this.selectedTag(ustensil)
             })
-
         })
         
-       
-              
         btnGroup.append(button,ul)
-        dropdowns.append(btnGroup )
+        dropdowns.append(btnGroup)
     
         return dropdowns
-    }  
-    selectedTag(ustensil){
-        this.selectItemCallback({type:'ustensil', label:ustensil})
-        
     }
-   
+    selectedTag(ustensil){
+        this.selectItemCallback({type:'ustensil', label:ustensil})   
+    }
 }
